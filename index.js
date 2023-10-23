7e13292aa66c2f5ff3b7a537c9e277c8099598bf
EcoCoapi <ecocoapi@gmail.com>
2023-10-23 17:09:36 +0200

    6

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-user = process.env("MYSQL_ADDON_DB");
 
 app.get('/', (req, res) => {
-  res.send(user);
+  res.send(process.env("MYSQL_ADDON_DB"));
 });
 
 app.listen(port, () => {
