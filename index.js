03f27b14f90136211bc6f09f51370b67ef0dd43f
EcoCoapi <ecocoapi@gmail.com>
2023-10-23 17:42:07 +0200

    A

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const mysql = require('mysql');
 
