c0cf93f370c1b74c917a308dad74603a600bbe5b
EcoCoapi <146847588+EcoCoapi@users.noreply.github.com>
2023-11-29 16:46:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -695,7 +695,7 @@ app.put("/groupe/classe/add/:id", (req, res) => {
 
     const idGroupe= req.params.id
 
-    const {idClasse} = req.body
+    const {idClasse,listeClasse} = req.body
 
     let s = listeClasse + `${idClasse}|`
 
